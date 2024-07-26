c686d232ce0c7ed14fbedf6800009a8fc813c0ca
DongchannN <ldc991104@gmail.com>
2024-07-26 16:25:05 +0900

    chore : ft_cd.c에서 ft_export.c의 함수 사용으로 builtin.h 생성

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/minishell.h"
+#include "../../include/builtin.h"
 
 int	ft_cd(char *path, char **envv)
 {
